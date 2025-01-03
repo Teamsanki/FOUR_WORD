5f31d1848df183a1227c06bba3eccc7c0d2f08de
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2025-01-04 02:02:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")
+TOKEN = os.getenv("MTMyNDgzNzUyNzgwMjA4OTU1NA.GB9AYm.M3E-qwNN0yycJiY9ow6KBl_Jznpu3FtvW31vUA")
 
 # Bot setup
 intents = discord.Intents.default()
